17d25fe905baa8549bb5720e0a0a11fb11f8b260
97016086 Renate Pit <97016086@st.deltion.nl>
2021-11-08 08:47:54 +0100

    echo's neergezet met strings erin.

--- cirkel.php
+++ cirkel.php
@@ -1,10 +1,11 @@
 <?php
 
-readline: ("voer de diameter van een cirkel in:");
-
-if (readline )
+$readline= ("voer de diameter van een cirkel in:");
 
 
+
+echo "de omtrek van de cirkel is\n";
+echo "de oppervlakte van de cirkel is\n";
 
 
 
