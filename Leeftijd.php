5858049f11b95a62ee1d76970b785a950955a03a
97016086 Renate Pit <97016086@st.deltion.nl>
2021-11-28 14:46:45 +0100

    programma gemaakt met de variable met mijn leeftijd en een voor hoeveel jaar ouder ik ben dan 16 en jonger dan 50, hoelang het nog duurt totdat ik 67 ben en hoelang het nog duurt voordat ik 100 ben.

---
+++ Leeftijd.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$age=28;
+
+$ouder= $age - 16;
+$jonger= 50 - $age;
+$pensioen= 67- $age;
+$honderd= 100 -$age;
+echo "Renate is $ouder jaar ouder dan 16\n";
+echo "Het duurt nog $jonger jaar voordat Renate 50 jaar oud is\n";
+echo "Het duurt nog $pensioen jaar voordat Renate 67 jaar oud is\n.";
+echo "Het duurt nog $honderd jaar voordat Renate 100 jaar oud is";
+
+
+?>
