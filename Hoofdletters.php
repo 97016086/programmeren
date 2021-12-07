520b374541d1d3ff00f3e267613dcb604478fdca
97016086 Renate Pit <97016086@st.deltion.nl>
2021-12-07 11:57:16 +0100

    bestand aangemaakt en verschillende variabelen een van de dingen meegegeven voor hoofletter of wat dan ook.

---
+++ Hoofdletters.php
@@ -0,0 +1,20 @@
+<?php
+
+$zin = readline ('Voer een zin in:');
+$zin= strtoupper($zin);
+$zin2= strtolower($zin);
+$zin3= ucwords(strtolower($zin));
+$zin4=ucfirst(strtolower($zin));
+
+
+
+
+echo "$zin\n";
+echo "$zin2\n";
+echo "$zin3\n";
+echo $zin4;
+
+
+
+
+?>
