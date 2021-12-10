b331c236a108763c820e669045423efadc30b47e
97016086 Renate Pit <97016086@st.deltion.nl>
2021-12-10 10:38:33 +0100

    variabelen bedacht en deze een waarde heb gegeven. gekeken hoevaak een getal in een ander getal past.

--- deelbaar.php
+++ deelbaar.php
@@ -1,13 +1,11 @@
 <?php 
+$deeltal=readline("Geef een getal:");
+$deler=readline("Geef nog een getal:"); 
 
-readline("Geef een getal:");
-readline("Geef nog een getal:");
-
-
-
+ $delen=$deeltal/$deler;
 
 
-
+echo "De deler past $delen keer in het deeltal";
 
 
 
